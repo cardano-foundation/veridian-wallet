a513888ef49210ddc2b9e022e73205e8bf867ebc
J Caso <jcaso@protonmail.com>
2025-07-22 20:28:23 +0800

    fix: name fixture

--- src/core/__fixtures__/agent/multiSigFixtures.ts
+++ src/core/__fixtures__/agent/multiSigFixtures.ts
@@ -850,7 +850,7 @@ const linkedContacts = [
 ];
 
 const queuedIdentifier: QueuedGroupCreation & { initiator: true } = {
-  name: "1.2.0.3:0:1-groupid-testUser:Identifier 2",
+  name: "1.2.0.3:0:Identifier 2",
   data: inceptionDataFix,
   initiator: true,
   groupConnections: linkedContacts,
