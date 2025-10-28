b5264c99c1e134edcbe131897bd62835a9a94b5f
J Caso <jcaso@protonmail.com>
2025-10-28 13:34:42 +0100

    fix: logs in filesystem and verify path

--- src/utils/logger/strategies/LocalFileStrategy.ts
+++ src/utils/logger/strategies/LocalFileStrategy.ts
@@ -5,9 +5,31 @@ import { loggingConfig } from "../LoggingConfig";
 export class LocalFileStrategy implements ILogger {
   private logFile = loggingConfig.offlineLogFileName;
 
+  constructor() {
+    // Log the file name
+    // eslint-disable-next-line no-console
+    console.debug(`LocalFileStrategy initialized. Log file name: ${this.logFile}`);
+
+    // Log the full path using console.info
+    Filesystem.getUri({
+      directory: Directory.Data,
+      path: this.logFile,
+    }).then((result) => {
+      // eslint-disable-next-line no-console
+      console.info(`Full path to local log file: ${result.uri}`);
+    }).catch((e) => {
+      // If an error occurs here, we'll fall back to console.error.
+      // eslint-disable-next-line no-console
+      console.error(`Error getting URI for log file:`, e);
+    });
+
+    // To view the file content: 
+    // - iOS: Open Terminal and run `cat "<result.uri>"` (replace with actual URI logged above)
+    // - Android: Use Android Studio > View > Tool Windows > Device File Explorer > /data/data/org.cardanofoundation.idw/files/local-logs.txt
+  }
+
   async log(logEntry: ParsedLogEntry) {
     const entry = JSON.stringify(logEntry) + "\n";
-
     await Filesystem.appendFile({
       path: this.logFile,
       data: entry,
