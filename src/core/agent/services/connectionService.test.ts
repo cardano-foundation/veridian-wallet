0e42e01d7916bb6dfb5b9354307499db875d07c9
Sotatek-DucPhung <duc.phung@sotatek.com>
2025-10-29 15:29:09 +0700

    fix(tests): remove optional chaining for historyItems in connectionService tests

--- src/core/agent/services/connectionService.test.ts
+++ src/core/agent/services/connectionService.test.ts
@@ -1991,7 +1991,7 @@ describe("Connection service of agent", () => {
     });
     expect(result.historyItems).toHaveLength(1);
     expect(
-      result.historyItems?.some(
+      result.historyItems.some(
         (item) => item.type === ConnectionHistoryType.IPEX_AGREE_COMPLETE
       )
     ).toBe(false);
