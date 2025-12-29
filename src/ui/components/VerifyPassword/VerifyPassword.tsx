9e30ccf88ef6da21a7ae811bfd46b7a18db01256
Sotatek-DukeVu <vuvanduc@Vus-MacBook-Pro.local>
2025-12-29 14:36:27 +0700

    fix(ui): enable confirm button when password length greater than 8

--- src/ui/components/VerifyPassword/VerifyPassword.tsx
+++ src/ui/components/VerifyPassword/VerifyPassword.tsx
@@ -172,7 +172,7 @@ const VerifyPassword = ({
         <PageFooter
           primaryButtonAction={() => setAttempts(attempts - 1)}
           primaryButtonText={`${i18n.t("verifypassword.confirm")}`}
-          primaryButtonDisabled={verifyPasswordValue.length === 0}
+          primaryButtonDisabled={verifyPasswordValue.length < 8}
         />
       </OptionModal>
       <Alert
