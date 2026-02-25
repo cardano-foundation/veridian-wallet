0843a606736069fbb7a8fa828708bc061aac7f6e
Sotatek-DukeVu <vuvanduc@Vus-MacBook-Pro.local>
2026-02-25 11:25:30 +0700

    feat(ui): replace blurry images

--- src/ui/pages/ProfileSetup/components/Welcome/Welcome.tsx
+++ src/ui/pages/ProfileSetup/components/Welcome/Welcome.tsx
@@ -1,17 +1,13 @@
 import { IonContent } from "@ionic/react";
 import { FinishSetupProps } from "./Welcome.types";
 import { i18n } from "../../../../../i18n";
-import WelcomeImage from "../../../../assets/images/welcome.png";
 import "./Welcome.scss";
 
 const Welcome = ({ userName }: FinishSetupProps) => {
   return (
     <IonContent className="finish-setup">
       <div className="container">
-        <img
-          src={WelcomeImage}
-          alt="welcome"
-        />
+        <span className="welcome-icon">🎉</span>
         <h3>
           {i18n.t("setupprofile.finishsetup.greeting", { name: userName })}
         </h3>
