429b7a4cffd60251e9c265e756800e809e4317e9
Sotatek-DukeVu <vuvanduc@Vus-MacBook-Pro.local>
2026-03-03 14:06:25 +0700

    fix(ui): update blurry image

--- src/ui/pages/ProfileSetup/components/Welcome/Welcome.tsx
+++ src/ui/pages/ProfileSetup/components/Welcome/Welcome.tsx
@@ -1,13 +1,18 @@
 import { IonContent } from "@ionic/react";
 import { FinishSetupProps } from "./Welcome.types";
 import { i18n } from "../../../../../i18n";
+import WelcomeImg from "../../../../assets/images/welcome.svg";
 import "./Welcome.scss";
 
 const Welcome = ({ userName }: FinishSetupProps) => {
   return (
     <IonContent className="finish-setup">
       <div className="container">
-        <span className="welcome-icon">🎉</span>
+        <img
+          className="welcome-icon"
+          src={WelcomeImg}
+          alt="Welcome"
+        />
         <h3>
           {i18n.t("setupprofile.finishsetup.greeting", { name: userName })}
         </h3>
